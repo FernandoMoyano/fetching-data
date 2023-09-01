09152f7f8fe19885f99075fa4396b04be4ad89f5
FernandoMoyano <fernandomoyano21@gmail.com>
2023-09-01 19:13:45 -0300

    fetching-data

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState(null);
+  
+  useEffect(()=>{
+    
+  },[])
+  return <div>Hola Mundo</div>;
+}
+
+export default App;
