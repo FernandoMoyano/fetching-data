26012fed6a5442d9985551e31cd18caff305f91a
FernandoMoyano <fernandomoyano21@gmail.com>
2023-09-01 19:24:19 -0300

    fetch-pomise

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,24 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState(null);
-  
-  useEffect(()=>{
-    
-  },[])
-  return <div>Hola Mundo</div>;
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+  return (
+    <div>
+      <h3>Fetching</h3>
+      <div>
+        <ul>
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
