9326ba2f132c137237730b87579bdac97f5f3357
FernandoMoyano <fernandomoyano21@gmail.com>
2023-09-01 22:49:34 -0300

    AbortController

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import useFetch from "./useFetch";
 import "./App.css";
 
 function App() {
-  const { data, loading, error } = useFetch(
+  const { data, loading, error, handleCancelRequest } = useFetch(
     "https://jsonplaceholder.typicode.com/users"
   );
 
   return (
     <div>
       <h3>Fetching</h3>
+      <button onClick={handleCancelRequest}>Cancel Request</button>
       <div>
         <ul>
           {error && <li>Error:{error}</li>}
--- src/useFetch.js
+++ src/useFetch.js
@@ -4,17 +4,36 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [controller, setController] = useState(null);
 
   useEffect(() => {
+    const abortController = new AbortController();
+    setController(abortController);
     setLoading(true);
-    fetch(url)
+
+    fetch(url, { signal: abortController.signal })
       .then((response) => response.json())
       .then((data) => setData(data))
-      .catch((error) => setError(error))
+      .catch((error) => {
+        if (error.name === "AbortError") {
+          console.log("Request cancelled");
+        } else {
+          setError(error);
+        }
+      })
       .finally(() => setLoading(false));
+
+    return () => abortController.abort();
   }, []);
 
-  return { data, loading, error };
+  const handleCancelRequest = () => {
+    if (controller) {
+      controller.abort();
+      setError("Request cancelled");
+    }
+  };
+
+  return { data, loading, error, handleCancelRequest };
 };
 
 export default useFetch;
